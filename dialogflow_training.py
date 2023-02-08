0bfddcba34fa3e3caaf120907995025bbca096ae
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-02-08 21:14:55 +0300

    add DialogFlow learning script

---
+++ dialogflow_training.py
@@ -0,0 +1,48 @@
+import json
+
+from environs import Env
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    env = Env()
+    env.read_env()
+    project_id = env.str('PROJECT_ID')
+    with open('questions.json', 'r') as questions:
+        dump = json.loads(questions.read())
+    for display_name, phrases in dump.items():
+        questions = phrases['questions']
+        answer = [phrases['answer']]
+        create_intent(project_id, display_name, questions, answer)
+
+
+if __name__ == '__main__':
+    main()
