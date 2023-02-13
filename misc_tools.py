1a9d3e6805278c07b69c0dd4956a02d054192344
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-02-13 18:43:02 +0300

    delete print

--- misc_tools.py
+++ misc_tools.py
@@ -6,7 +6,6 @@ from google.cloud import dialogflow
 def detect_intent_texts(project_id, session_id, text, language_code):
     client = dialogflow.SessionsClient()
     session = client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.QueryInput(text=text_input)
